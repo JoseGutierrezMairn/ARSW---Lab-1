7f6dd6a3208fa8f7e2a4116a18ea83af372d5583
Jose Gutierrez Marin <andres.gutierrez-m@mail.escuelaing.edu.co>
2021-01-21 16:55:37 -0500

    ParteII propuesta solucion sin join

--- src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
@@ -6,6 +6,8 @@
 package edu.eci.arsw.blacklistvalidator;
 
 import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
@@ -28,26 +30,47 @@ public class HostBlackListsValidator {
      * NOT Trustworthy, and the list of the five blacklists returned.
      * @param ipaddress suspicious host's IP address.
      * @return  Blacklists numbers where the given host's IP address was found.
+     * @throws InterruptedException 
      */
-    public List<Integer> checkHost(String ipaddress){
+    public List<Integer> checkHost(String ipaddress, int n) throws InterruptedException{
         
         LinkedList<Integer> blackListOcurrences=new LinkedList<>();
         
         int ocurrencesCount=0;
         
         HostBlacklistsDataSourceFacade skds=HostBlacklistsDataSourceFacade.getInstance();
-        
+        List<Seeker> seekers = new ArrayList<Seeker>();
+        List<Integer> alives = new ArrayList<Integer>();
         int checkedListsCount=0;
+        int segment = (int) Math.ceil(skds.getRegisteredServersCount()/n);
+        int b = 0;
+        int e = b + segment;
+        for (int i = 0; i<n; i++) {
+        	Seeker s = new Seeker(skds, e, b, BLACK_LIST_ALARM_COUNT, ipaddress);
+        	s.start();
+        	seekers.add(s);
+        	b = e + 1;
+        	e+=segment+1;
+        }
         
-        for (int i=0;i<skds.getRegisteredServersCount() && ocurrencesCount<BLACK_LIST_ALARM_COUNT;i++){
-            checkedListsCount++;
-            
-            if (skds.isInBlackListServer(i, ipaddress)){
-                
-                blackListOcurrences.add(i);
-                
-                ocurrencesCount++;
-            }
+        int x = 0;
+        while (alives.size() < n) {
+        	if(x == n) {
+        		x = 0;
+        		
+        	}else{
+        		if(!seekers.get(x).isAlive() && !alives.contains(x)) {
+        			alives.add(x);
+        		}
+        		x++;
+        	}
+        }
+        for (Seeker s : seekers) {
+        	checkedListsCount+= s.getCheckedListsCount();
+        	ocurrencesCount+= s.getOccurrences();
+        	for (Integer inte :  s.getListOccurrences()) {
+        		 blackListOcurrences.add(inte);
+        	}
         }
         
         if (ocurrencesCount>=BLACK_LIST_ALARM_COUNT){
